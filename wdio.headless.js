7e61b53b76e8c256110b7073fc6319dc105f648b
Anastasiya Voropayeva <voropayeva.a@gmail.com>
2025-02-07 16:39:05 +0200

    Fixed Automation Tests

---
+++ wdio.headless.js
@@ -0,0 +1,15 @@
+const {config: mainConfig} = require ("./wdio.conf.js");
+
+exports.config = {
+    ...mainConfig,
+    ...{
+      capabilities: [
+        {
+         browserName: "chrome",
+         "goog:chromeOptions": {
+           args: ["headless", "disable-gpu"],
+                },
+             },
+         ],  
+    },
+};
