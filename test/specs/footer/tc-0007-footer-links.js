1db5ecdc0217d425777d31457869ad9670012306
anavoro <anastasiia.v@misolla-music.com>
2025-02-11 19:48:34 +0200

    Adjusted code of Footer test

--- test/specs/footer/tc-0007-footer-links.js
+++ test/specs/footer/tc-0007-footer-links.js
@@ -1,14 +1,14 @@
-const loginTestPage = require('../../pageobjects/02-logintestpage.js');
-const inventoryPage = require('../../pageobjects/03-inventorypage.js');
+const loginTestPage = require("../../pageobjects/02-logintestpage.js");
+const inventoryPage = require("../../pageobjects/03-inventorypage.js");
 
-describe('Footer Links Test', () => {
-    it('should open footer links in a new tab and navigate correctly', async () => {
-        await loginTestPage.open();
-        await loginTestPage.login('standard_user', 'secret_sauce');
-        await expect(browser).toHaveUrl(inventoryPage.url);
+describe("Footer", () => {
+  it("should open footer links in a new tab and navigate correctly", async () => {
+    await loginTestPage.open();
+    await loginTestPage.login("standard_user", "secret_sauce");
+    await expect(browser).toHaveUrl(inventoryPage.url);
 
-        for (const footerLink of inventoryPage.footerLinks) {
-            await inventoryPage.clickFooterLink(footerLink);
-        }
-    });
+    for (const footerLink of inventoryPage.footerLinks) {
+      await inventoryPage.clickFooterLink(footerLink);
+    }
+  });
 });
