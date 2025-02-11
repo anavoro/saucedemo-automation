14ccde8c931876425a72b40cef0c3565a65c2a6d
anavoro <anastasiia.v@misolla-music.com>
2025-02-11 19:50:28 +0200

    Improved codestyle of Checkout tests

--- test/specs/checkout/tc-0008-valid-checkout.js
+++ test/specs/checkout/tc-0008-valid-checkout.js
@@ -1,35 +1,29 @@
-const loginTestPage = require('../../pageobjects/02-logintestpage.js');
-const cartPage = require('../../pageobjects/04-cartpage.js'); 
-const checkoutPage = require ('../../pageobjects/05-checkoutpage.js');
-const inventoryPage = require ('../../pageobjects/03-inventorypage.js')
-const { faker } = require('@faker-js/faker');
+const loginTestPage = require("../../pageobjects/02-logintestpage.js");
+const cartPage = require("../../pageobjects/04-cartpage.js");
+const checkoutPage = require("../../pageobjects/05-checkoutpage.js");
+const inventoryPage = require("../../pageobjects/03-inventorypage.js");
+const { faker } = require("@faker-js/faker");
 
-describe('Valid Chekout', () => {
-    it('should fill out all information and checkout successfully', async () => {
+describe("Checkout", () => {
+  it("should fill out all information and checkout successfully", async () => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const zipCode = faker.location.zipCode();
 
-        const firstName = faker.person.firstName();
-        const lastName = faker.person.lastName();
-        const zipCode = faker.location.zipCode();
+    await loginTestPage.open();
+    await loginTestPage.login("standard_user", "secret_sauce");
+    await expect(browser).toHaveUrl(inventoryPage.url);
 
-        await loginTestPage.open();
-        await loginTestPage.login('standard_user', 'secret_sauce');
-        await expect(browser).toHaveUrl(inventoryPage.url);
+    await inventoryPage.addItemToCart();
+    await inventoryPage.goToCart();
+    await cartPage.proceedToCheckout();
+    await checkoutPage.fillOutCheckoutDetails(firstName, lastName, zipCode);
+    await checkoutPage.finishCheckout();
+    await checkoutPage.returnToProducts();
 
-        await inventoryPage.addItemToCart();
+    await expect(browser).toHaveUrl(inventoryPage.url);
 
-        await inventoryPage.goToCart();
-
-        await cartPage.proceedToCheckout();
-        
-        await checkoutPage.fillOutCheckoutDetails(firstName, lastName, zipCode);
-
-        await checkoutPage.finishCheckout();
-        await checkoutPage.returnToProducts();
-        
-        await expect(browser).toHaveUrl(inventoryPage.url);
-
-        const cartItemCount = await inventoryPage.getItemCount();
-        expect(cartItemCount).toBe(0);
-
-    });
+    const cartItemCount = await inventoryPage.getItemCount();
+    expect(cartItemCount).toBe(0);
+  });
 });
--- test/specs/checkout/tc-0009-checkout-without-products.js
+++ test/specs/checkout/tc-0009-checkout-without-products.js
@@ -1,24 +1,23 @@
-const loginTestPage = require('../../pageobjects/02-logintestpage.js');
-const checkoutPage = require ('../../pageobjects/05-checkoutpage.js');
-const inventoryPage = require ('../../pageobjects/03-inventorypage.js')
-const cartPage = require('../../pageobjects/04-cartpage.js');
+const loginTestPage = require("../../pageobjects/02-logintestpage.js");
+const checkoutPage = require("../../pageobjects/05-checkoutpage.js");
+const inventoryPage = require("../../pageobjects/03-inventorypage.js");
+const cartPage = require("../../pageobjects/04-cartpage.js");
 
-describe('Invalid Checkout', () => {
-    it('should not proceed without any products in a cart', async () => {
+describe("Checkout", () => {
+  it("should not proceed without any products in a cart", async () => {
+    await loginTestPage.open();
+    await loginTestPage.login("standard_user", "secret_sauce");
 
-        await loginTestPage.open();
-        await loginTestPage.login('standard_user', 'secret_sauce');
-        await expect(browser).toHaveUrl(inventoryPage.url);
+    await expect(browser).toHaveUrl(inventoryPage.url);
 
-        await inventoryPage.goToCart()
+    await inventoryPage.goToCart();
+    const itemCount = await inventoryPage.getItemCount();
 
-        const itemCount = await inventoryPage.getItemCount();
-        expect(itemCount).toBe(0);
+    expect(itemCount).toBe(0);
 
-        await cartPage.proceedToCheckout();
+    await cartPage.proceedToCheckout();
+    const errorMessageText = await checkoutPage.errorMessage.getText();
 
-        const errorMessageText = await checkoutPage.errorMessage.getText();  
-        expect(errorMessageText).toContain('Cart is empty');
-
-    });
+    expect(errorMessageText).toContain("Cart is empty");
+  });
 });
